68055ecf36d3884f238ba999ada5ed5ee935f6f3
Pablo Lizardo <pablo_lizardo@yahoo.com.ar>
2017-10-17 15:29:43 -0300

    resume php

---
+++ config.php
@@ -0,0 +1,50 @@
+<?php
+
+$personal['first_name']= 'Pablo';
+$personal['last_name'] = 'Lizardo';
+$personal['phone_number'] = '2964 15614417';
+$personal['age'] = '33';
+$personal['city'] = 'Río Grande, Tierra del Fuego';
+$personal['email_address'] = 'lizardo.pablo@gmail.com';
+
+$social_media[] = array('Facebook', 'http://facebook.com/pablolizardo');
+$social_media[] = array('Twitter', 'http://twitter.com/pablolizardo');
+$social_media[] = array('Github', 'http://github.com/pablolizardo');
+
+$work_history[] = array('2015 - 2017', 'Full-stack Developer', 'TVFUEGO', 'Laravel, JavaScript , PHP, Angular.');
+$work_history[] = array('2013 - 2017', 'Senior Game Designer at Ponup', 'Ponup, Ltd', 'GDScript, Python , Phaser, Javascript ,Canvas');
+$work_history[] = array('2013 - 2017', 'Web Developer and Graphic Designer', 'SOKI', 'Laravel, JavaScript , PHP, Angular, Cordova, Web Services', 'www.soki.com.ar');
+
+
+$education[] = array('Universidad Tecnológica Nacional', 'Tecnicatura superior, Programación, Próximo a Recibirme');
+$education[] = array('Centro de Estudios Terciarios N° 35', 'Tecnico Superior en Diseño Grafico, Completo');
+$education[] = array('Universidad Nacional del Litoral', 'Programación orientada a Videojuegos, Incompleto');
+$education[] = array('Universidad de Morón', 'Comunicacion Audiovisual, Facultad de Artes y Diseño, Incompleto');
+
+// Presumably you learned something in school and developed a few skills over the course of your lifetime.  Let's add them here.  
+$skills[] = array('Desarrollo', 'C, C#, C++, Java, Javascript, PHP, Python, Visual Basic .NET, Ruby, Phaser, GDScript');
+$skills[] = array('Web', 'Angular, jQuery, Ajax, Laravel, Wordpress, Joomla, Drupal, HTML5+ , CSS, SCSS.');
+$skills[] = array('Bases de Datos', 'mySql, SQL, OracleDB, SQLlite, MariaDB.');
+$skills[] = array('Sistemas', 'GNU/Linux (Especializado en RedHat -> Fedora/CentOs), BASH Shell, SysAdmin, Grunt, Bower, Git, SVN.');
+$skills[] = array('UI/UX', 'Blender, Inkscape, Gimp, Scribus, Glade, Bootstrap 3+.');
+// $skills[] = array('Software', 'Programming learning junkie in Ruby, Rails, HTML5/CSS3, Java, Clojure. TDD advocate.');
+$skills[] = array('Idiomas', 'Spanish (nativo), English (fluido)');
+
+$awards[] = array('Quito - Ecuador, 2013', 'En el encuentro MINGA Por La Libertad Tecnológica di la charla de apertura sobre el uso de herramientas open source en producciones profesionales .');
+$awards[] = array('Quito - Ecuador, 2013', 'En la Universidad La Metro de Artes Audiovisuales di una conferencia sobre el uso de Software Libre para la creacion de la pelicula Cronopios y otras producciones');
+$awards[] = array('Nueva York - EEUU, 2010', 'En el marco del encuentro de usuarios de blender de Nueva York (NYUM) di una charla sobre la pelicula Tube (Wires for Empathy).');
+$awards[] = array('San Luis - Argentina, 2012', 'Durante el evento de San Luis Digital di dos talleres sobre el uso de herramientas libres para la produccion de 3d en Cine y Publicidad.');
+$awards[] = array('Cordoba - Argentina, 2011', 'Di una charla en la Facultad de Ingenieria sobre el uso de Software Libre para produccion de personajes y animacion.');
+$awards[] = array('Buenos Aires - Argentina, 2011', 'Di varias charlas en eventos relacionados a la produccion de contenidos en 3D utilizando herramientas libres como Blender.');
+$awards[] = array('Harvard University - Boston - 2010', 'Participe como oyente durante los 2 dias de disertaciones en el Evento "Libre Planet" con importantes oradores sobre tecnologias libres y desarrollo utilizando herramientas Open Source.');
+
+$settings['social_media'] = true;
+$settings['education'] = true;
+$settings['skills'] = true;
+$settings['awards'] = true;
+
+$settings['printable'] = true;
+$settings['style'] = "light";
+$settings['lang'] = "es";
+?>
+
---
+++ lang/es.php
@@ -0,0 +1,12 @@
+<?php
+$lang['title'] = $personal['first_name'] . ' ' . $personal['last_name'] . " : CV";
+$lang['header'] = $personal['first_name'] . ' ' . $personal['last_name'];
+$lang['print'] = "Imprimir";
+$lang['contact'] = "Contacto";
+$lang['contact_desc'] = "Full Stack Developer.";
+$lang['phone'] = "Teléfono";
+$lang['email'] = "Email";
+$lang['education'] = "Educación";
+$lang['skills'] = "Habilidades";
+$lang['awards'] = "Disertaciones";
+?>
